6e6f962d552e31bd01475ba446cf9bc4965e3f55
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-05-02 15:46:04 +0900

    Update .lua

--- .lua
+++ .lua
@@ -20,6 +20,10 @@ local rd3Exploit = loadstring(game:HttpGet("https://raw.githubusercontent.com/Si
 local TeleportService = game:GetService("TeleportService")
 local TextChatService = game:GetService("TextChatService")
 local Stats = game:GetService("Stats")
+local getinfo = getinfo or debug.getinfo
+local DEBUG = false
+local Hooked = {}
+local Detected, Kill
 local HTMLcolors = { 
     ["Red"] = "rgb(255, 0, 0)",
     ["Yellow"] = "rgb(255, 255, 0)",
@@ -323,6 +327,59 @@ function lib:CustomTeleport(mthd,str,tip)
 	else
 		lib:notify(":18927: Teleport method is invalid, try select another method",10)
 	end
+end
+
+function lib:ACPatch()
+setthreadidentity(2)
+
+for i, v in getgc(true) do
+    if typeof(v) == "table" then
+        local DetectFunc = rawget(v, "Detected")
+        local KillFunc = rawget(v, "Kill")
+    
+        if typeof(DetectFunc) == "function" and not Detected then
+            Detected = DetectFunc
+            
+            local Old; Old = hookfunction(Detected, function(Action, Info, NoCrash)
+                if Action ~= "_" then
+                    if DEBUG then
+                        warn(`Adonis AntiCheat flagged\nMethod: {Action}\nInfo: {Info}`)
+                    end
+                end
+                
+                return true
+            end)
+
+            table.insert(Hooked, Detected)
+        end
+
+        if rawget(v, "Variables") and rawget(v, "Process") and typeof(KillFunc) == "function" and not Kill then
+            Kill = KillFunc
+            local Old; Old = hookfunction(Kill, function(Info)
+                if DEBUG then
+                    warn(`Adonis AntiCheat tried to kill (fallback): {Info}`)
+                end
+            end)
+
+            table.insert(Hooked, Kill)
+        end
+    end
+end
+
+local Old; Old = hookfunction(getrenv().debug.info, newcclosure(function(...)
+    local LevelOrFunc, Info = ...
+
+    if Detected and LevelOrFunc == Detected then
+        if DEBUG then
+            warn(`zins | adonis bypassed`)
+        end
+
+        return coroutine.yield(coroutine.running())
+    end
+    
+    return Old(...)
+end))
+setthreadidentity(7)
 end
 
 local function SendMessage(url,message)
