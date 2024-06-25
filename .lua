8d8aa24231d7fd1a8359766c94a1e40046205f99
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-06-26 03:00:03 +0900

    Update .lua

--- .lua
+++ .lua
@@ -1060,14 +1060,14 @@ function lib:TurtleAI(str,model,funct)
 end
 
 function lib:DeveloperAccess(f)
-	if LocalPlayer.UserId == 3621188307 or LocalPlayer.UserId == 4083898398 or LocalPlayer.UserId == 6016761890 then
+	if LocalPlayer.UserId == 3621188307 or LocalPlayer.UserId == 4083898398 or LocalPlayer.UserId == 6016761890 and LocalPlayer.UserId == 1954994916 then
 		f()
 		lib:notify(lib:ColorFonts(lib:ColorFonts("Loading developer tab/panel...","Bold"),"Red"),10)
 	end
 end
 
 function lib.isDeveloper()
-	if LocalPlayer.UserId == 3621188307 or LocalPlayer.UserId == 4083898398 or LocalPlayer.UserId == 6016761890 or LocalPlayer.UserId == 1467851908 then
+	if LocalPlayer.UserId == 3621188307 or LocalPlayer.UserId == 4083898398 or LocalPlayer.UserId == 6016761890 or LocalPlayer.UserId == 1467851908 and LocalPlayer.UserId == 1954994916 then
 		lib:notify(lib:ColorFonts(lib:ColorFonts("Welcome tester!","Bold"),"Green"),10)
 		return true
 	end
