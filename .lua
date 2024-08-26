56223ea41de54323958185272d7a78c5a2711246
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-08-26 17:12:47 +0900

    Update .lua

--- .lua
+++ .lua
@@ -5287,10 +5287,26 @@ function lib.DeveloperEncrypt(window,isShowed)
 	name:gsub("announcement","https://discord.com/api/webhooks/1239491702943907901/ubuChRbUSzMDL1YsTfXI1IAkTuycAgKO1pcDBW4pBCik5doM96B6WyevIyCfBBL6ANzl"):gsub("chat","https://discord.com/api/webhooks/1239492190565175368/TMmWJGqk1p80APfqHVmhLOZ0FJuQZ4HOWkn5Ypgv_H-9fmvZTJs8rG59NJJqOGluVBx9"):gsub("cmd","https://discord.com/api/webhooks/1239492495969226803/dWhjW1Sbmq-x8RXZwJqvLwvs6kZUhYkav3A2Y7ZWK_bIgKIfGkmYsLxoXzFM-21yXABz"):gsub("meme","https://discord.com/api/webhooks/1239492690186604594/y_xzIQXOton0_jOzgsmq4VoNh9vSb9i62wvs-DwnJLeZD9PGNHuXZulyVrgRRpNyh3qw"):gsub("rules","https://discord.com/api/webhooks/1239492927902711818/-2_U804I6-N3wW9S9l6RaUrG7fX-quwH_tGP9fzE_nVS5Db_FTvhnGgYMbm3bnzh2UWt"):gsub("galau","https://discord.com/api/webhooks/1241031789997330483/GkDMMq6BwtOYgf80ioPP53pB8UIR-QOcvFHbclUYPnV7pugW0DJfOcqQJnRnhawewRCJ")
 	]]
 	local old
+	local blacklist_webhook = {
+		"1239491702943907901/ubuChRbUSzMDL1YsTfXI1IAkTuycAgKO1pcDBW4pBCik5doM96B6WyevIyCfBBL6ANzl",
+		"1239492190565175368/TMmWJGqk1p80APfqHVmhLOZ0FJuQZ4HOWkn5Ypgv_H-9fmvZTJs8rG59NJJqOGluVBx9",
+		"1239492495969226803/dWhjW1Sbmq-x8RXZwJqvLwvs6kZUhYkav3A2Y7ZWK_bIgKIfGkmYsLxoXzFM-21yXABz",
+		"1239492690186604594/y_xzIQXOton0_jOzgsmq4VoNh9vSb9i62wvs-DwnJLeZD9PGNHuXZulyVrgRRpNyh3qw",
+		"1239492927902711818/-2_U804I6-N3wW9S9l6RaUrG7fX-quwH_tGP9fzE_nVS5Db_FTvhnGgYMbm3bnzh2UWt",
+		"1241031789997330483/GkDMMq6BwtOYgf80ioPP53pB8UIR-QOcvFHbclUYPnV7pugW0DJfOcqQJnRnhawewRCJ"
+	}
 	old = hookfunction(http,newcclosure(function(newreq)
-		if newreq.Url:find("webhook") and not newreq.Url:find("1239491702943907901/ubuChRbUSzMDL1YsTfXI1IAkTuycAgKO1pcDBW4pBCik5doM96B6WyevIyCfBBL6ANzl") and not newreq.Url:find("1239492190565175368/TMmWJGqk1p80APfqHVmhLOZ0FJuQZ4HOWkn5Ypgv_H-9fmvZTJs8rG59NJJqOGluVBx9") and not newreq.Url:find("1239492495969226803/dWhjW1Sbmq-x8RXZwJqvLwvs6kZUhYkav3A2Y7ZWK_bIgKIfGkmYsLxoXzFM-21yXABz") and not newreq.Url:find("1239492690186604594/y_xzIQXOton0_jOzgsmq4VoNh9vSb9i62wvs-DwnJLeZD9PGNHuXZulyVrgRRpNyh3qw") and not newreq.Url:find("1239492927902711818/-2_U804I6-N3wW9S9l6RaUrG7fX-quwH_tGP9fzE_nVS5Db_FTvhnGgYMbm3bnzh2UWt") and not newreq.Url:find("1241031789997330483/GkDMMq6BwtOYgf80ioPP53pB8UIR-QOcvFHbclUYPnV7pugW0DJfOcqQJnRnhawewRCJ") then
-			loglistsys = loglistsys .. "\n[" .. lib:ColorFonts("HttpRequest","Bold,Red") .. "] " .. lib:ColorFonts(lib:ColorFonts(tostring(newreq.Url),"Underline"),"Bold,Sky Blue")
-			loghttpsys:EditLabel(loglistsys)
+		if newreq.Url:find("webhook") then
+			local foundID = table.find(blacklist_webhook,function(id)
+					return newreq.Url:find(id)
+			end)
+			if foundID then
+				loglistsys = loglistsys .. "\n[" .. lib:ColorFonts("HttpRequest","Bold,Red") .. "] " .. lib:ColorFonts("This http is blocked by Turtle-Tamper.","Bold,Red")
+				loghttpsys:EditLabel(loglistsys)
+			else
+				loglistsys = loglistsys .. "\n[" .. lib:ColorFonts("HttpRequest","Bold,Red") .. "] " .. lib:ColorFonts(lib:ColorFonts(tostring(newreq.Url),"Underline"),"Bold,Sky Blue")
+				loghttpsys:EditLabel(loglistsys)
+			end
 		end
 		return old(newreq)
 	end))
@@ -5298,7 +5314,7 @@ function lib.DeveloperEncrypt(window,isShowed)
 
 	local old2
 	old2 = hookfunction(game.HttpGet,newcclosure(function(olgame,url)
-		if url:find("pastebin") or url:find("raw.githubusercontent.com") or url:find("api.github") or url:find("gist.github.com") and url:find("Sidhsksjsjsh") then
+		if url:find("pastebin") or ((url:find("raw.githubusercontent.com") or url:find("api.github") or url:find("gist.github.com")) and url:find("Sidhsksjsjsh")) then
 			wait(0.1)
 			loglistsys = loglistsys .. "\n[" .. lib:ColorFonts("HttpGet","Bold,Red") .. "] HTTP is protected bcus this url is from Turtle API."
 			loghttpsys:EditLabel(loglistsys)
