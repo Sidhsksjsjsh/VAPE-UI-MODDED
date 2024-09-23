c29e75de2814dfd0aa6796afd3bf4dfb5bb01296
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-09-23 21:55:30 +0900

    Update .lua

--- .lua
+++ .lua
@@ -6099,22 +6099,7 @@ function lib.DeveloperEncrypt(window,isShowed)
 		musiclib:Toggle("Play music",false,function(value)
 					if value == true then
 						TurtleScreenNotify("Turtle Hub | Current Playing • Music",`Name : {MarketplaceService:GetProductInfo(tonumber(musichand.id)).Name}\nSound Id : {musichand.id}\nLength : {LocalPlayer.PlayerGui["TurtleMusic"]["TimeLength"]}`,{},nil,{})
-						debug.getmetatable(lib).__PLAY_MUSIC(
-							Id = musichand.id,
-							Volume = 1,
-							PlaybackSpeed = 1,
-							Looped = true,
-							TimePosition = 0,
-							Pitch = 1,
-							RollOffMode = Enum.RollOffMode["Linear"],
-							RollOffMaxDistance = 10000,
-							RollOffMinDistance = 10,
-							EmitterSize = 5,
-							DopplerScale = 1,
-							SoundGroup = nil,
-							EndTime = 0,
-							Parent = LocalPlayer.PlayerGui
-						)
+						debug.getmetatable(lib).__PLAY_MUSIC(Id = musichand.id,Volume = 1,PlaybackSpeed = 1,Looped = true,TimePosition = 0,Pitch = 1,RollOffMode = Enum.RollOffMode["Linear"],RollOffMaxDistance = 10000,RollOffMinDistance = 10,EmitterSize = 5,DopplerScale = 1,SoundGroup = nil,EndTime = 0,Parent = LocalPlayer.PlayerGui)
 					else
 						debug.getmetatable(lib).__STOP_MUSIC(LocalPlayer.PlayerGui)
 						TurtleScreenNotify("Turtle Hub | Music",`Music Stopped`,{},nil,{})
