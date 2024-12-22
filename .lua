597ee0925014ab40961beb2bdafeb3342f358558
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-12-22 21:59:21 +0900

    Update .lua

--- .lua
+++ .lua
@@ -3544,7 +3544,7 @@ function lib:Window(text, preset, closebind)
 	end
 
 	if table.find({Enum.Platform.IOS,Enum.Platform.Android},UserInputService:GetPlatform()) then
-		if game.PlaceId == 12135640403 or game.PlaceId == 15502802094 or game.PlaceId == 15742087191 or game.PlaceId == 18314395469 or game.PlaceId == 4972091010 or game.PlaceId == 7092693227 or game.PlaceId == 12137249458 then -- Anime Dungeon Fighters world 1 & 2 & 3, Evolution Simulator, Zombie Uprising, Gun Grounds FFA
+		if game.PlaceId == 12135640403 or game.PlaceId == 15502802094 or game.PlaceId == 15742087191 or game.PlaceId == 18314395469 or game.PlaceId == 4972091010 or game.PlaceId == 7092693227 or game.PlaceId == 12137249458 or game.PlaceId == 109644231059364 then -- Anime Dungeon Fighters world 1 & 2 & 3, Evolution Simulator, Zombie Uprising, Gun Grounds FFA, Maze Simulator
 			local THHUI = Instance.new("ScreenGui")
 			THHUI.Name = "VIP TURTLE HUB HIDE UI"
 			THHUI.Parent = lib.Interface("hide") --game:GetService("CoreGui")
